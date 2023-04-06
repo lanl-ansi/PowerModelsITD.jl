2c1b1337213fe6ec5d8998aab9e5cd18fce9c364
Juan Ospina <jjospina@lanl.gov>
2023-04-06 15:20:55 -0600

    ADD: boundary linking vars function and transmission constraints needed for decomposition process.

--- src/form_decomposition/boundary.jl
+++ src/form_decomposition/boundary.jl
@@ -80,3 +80,44 @@ function constraint_boundary_voltage_magnitude(pmd::_PMD.AbstractUnbalancedPower
     constraint_boundary_voltage_magnitude(pmd, i, t_bus, boundary["f_connections"], boundary["t_connections"]; nw=nw)
 
 end
+
+
+"""
+    function constraint_transmission_power_balance_boundary(
+        pm::_PM.AbstractPowerModel,
+        i::Int,
+        j::Int,
+        boundary_buses::Vector;
+        nw_pm::Int=nw_id_default
+    )
+
+General power balance contraints for boundary buses in the transmission system-side for decomposition.
+"""
+function constraint_transmission_power_balance_boundary(pm::_PM.AbstractPowerModel, i::Int, j::Int, boundary_buses::Vector; nw_pm::Int=nw_id_default)
+
+    bus = _PM.ref(pm, nw_pm, :bus, i)
+
+    boundary_pm = _PM.ref(pm, nw_pm, :boundary, j)
+    bus_pm = boundary_pm["f_bus"]
+
+    if bus["bus_i"] == bus_pm # consider Power flowing through respective boundary connection
+        bus_arcs = _PM.ref(pm, nw_pm, :bus_arcs, i)
+        bus_arcs_dc = _PM.ref(pm, nw_pm, :bus_arcs_dc, i)
+        bus_arcs_sw = _PM.ref(pm, nw_pm, :bus_arcs_sw, i)
+        bus_gens = _PM.ref(pm, nw_pm, :bus_gens, i)
+        bus_loads = _PM.ref(pm, nw_pm, :bus_loads, i)
+        bus_shunts = _PM.ref(pm, nw_pm, :bus_shunts, i)
+        bus_storage = _PM.ref(pm, nw_pm, :bus_storage, i)
+
+        bus_pd = Dict(k => _PM.ref(pm, nw_pm, :load, k, "pd") for k in bus_loads)
+        bus_qd = Dict(k => _PM.ref(pm, nw_pm, :load, k, "qd") for k in bus_loads)
+
+        bus_gs = Dict(k => _PM.ref(pm, nw_pm, :shunt, k, "gs") for k in bus_shunts)
+        bus_bs = Dict(k => _PM.ref(pm, nw_pm, :shunt, k, "bs") for k in bus_shunts)
+
+        bus_arcs_boundary_from = _PM.ref(pm, nw_pm, :bus_arcs_boundary_from, i)
+        constraint_transmission_power_balance(pm, nw_pm, j, i, bus_arcs, bus_arcs_dc, bus_arcs_sw, bus_gens, bus_storage, bus_pd, bus_qd, bus_gs, bus_bs, bus_arcs_boundary_from)
+        push!(boundary_buses, bus_pm) # add to vector storage
+    end
+end
+
