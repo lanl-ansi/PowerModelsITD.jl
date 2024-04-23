cea0e6a1eb76f5bf4700a90fff65b2482a54b5fc
Juan Ospina <jjospina@lanl.gov>
2024-04-23 10:36:32 -0600

    ADD: scaling to ACR-ACRU decomposition formulation.

--- src/form_decomposition/acr.jl
+++ src/form_decomposition/acr.jl
@@ -89,10 +89,46 @@ function constraint_boundary_voltage_magnitude(pmd::_PMD.ACRUPowerModel, ::Int, 
     vi_source = _PMD.var(pmd, nw, :vi, t_bus)
 
     # Add constraint(s): r->real, i->imaginary
-    JuMP.@constraint(pmd.model, vr_source[1]^2+vi_source[1]^2 == vr_source[2]^2+vi_source[2]^2)
-    JuMP.@constraint(pmd.model, vr_source[1]^2+vi_source[1]^2 == vr_source[3]^2+vi_source[3]^2)
+    JuMP.@constraint(pmd.model, (vr_source[1]^2+vi_source[1]^2) == (vr_source[2]^2+vi_source[2]^2))
+    JuMP.@constraint(pmd.model, (vr_source[1]^2+vi_source[1]^2) == (vr_source[3]^2+vi_source[3]^2))
 
 end
+
+
+
+"""
+    function constraint_transmission_boundary_power_shared_vars_scaled(
+        pm::_PM.AbstractACPModel,
+        i::Int;
+        nw::Int = nw_id_default
+    )
+
+ACRU power shared variables scaling constraints.
+"""
+function constraint_transmission_boundary_power_shared_vars_scaled(pm::_PM.ACRPowerModel, i::Int; nw::Int=nw_id_default)
+
+    boundary = _PM.ref(pm, nw, :boundary, i)
+    f_bus = boundary["f_bus"] # convention: from bus Transmission always!
+    t_bus = boundary["t_bus"] # convention: to bus Distribution always!
+
+    # Pbound_load vars
+    f_idx = (i, f_bus, t_bus)
+    pbound_load = _PM.var(pm, nw, :pbound_load, f_idx)
+    qbound_load = _PM.var(pm, nw, :qbound_load, f_idx)
+
+    pbound_load_scaled = _PM.var(pm, nw, :pbound_load_scaled, f_idx)
+    qbound_load_scaled = _PM.var(pm, nw, :qbound_load_scaled, f_idx)
+
+    # Get the base power conversion factor for T&D boundary connection
+    base_conv_factor = boundary["base_conv_factor"]
+
+    # Add scaling constraint
+    JuMP.@constraint(pm.model, pbound_load_scaled[1] == pbound_load[1]*(base_conv_factor))
+    JuMP.@constraint(pm.model, qbound_load_scaled[1] == qbound_load[1]*(base_conv_factor))
+
+end
+
+
 
 # TODO: multinetwork compatibility by using nw info.
 """
@@ -105,8 +141,9 @@ end
     )
 
 Generates the ACR-ACRU boundary linking vars vector to be used by the IDEC Optimizer.
+The parameter `export_models` is a boolean that determines if the JuMP models' shared variable indices are exported to the pwd as `.nl` files.
 """
-function generate_boundary_linking_vars(pm::_PM.ACRPowerModel, pmd::_PMD.ACRUPowerModel, boundary_number::String; nw::Int=nw_id_default)
+function generate_boundary_linking_vars(pm::_PM.ACRPowerModel, pmd::_PMD.ACRUPowerModel, boundary_number::String; nw::Int=nw_id_default, export_models::Bool=false)
 
     # Parse to Int
     boundary_number = parse(Int64, boundary_number)
@@ -131,10 +168,25 @@ function generate_boundary_linking_vars(pm::_PM.ACRPowerModel, pmd::_PMD.ACRUPow
     Vi = _PM.var(pm, nw, :vi, f_bus)
 
     # Transmission: Pload & Qload (master)
-    P_load = _PM.var(pm, nw, :pbound_load, f_idx)
-    Q_load = _PM.var(pm, nw, :qbound_load, f_idx)
+    P_load = _PM.var(pm, nw, :pbound_load_scaled, f_idx)
+    Q_load = _PM.var(pm, nw, :qbound_load_scaled, f_idx)
 
     boundary_linking_vars = [[P_load[1], Q_load[1], Vr, Vi], [p_aux[1], q_aux[1], vr[1], vi[1]]]
+
+    if (export_models == true)
+        # Open file where shared vars indices are going to be written
+        file = open("shared_vars.txt", "a")
+        # Loop through the vector of shared variables
+        for sh_vect in boundary_linking_vars
+            for sh_var in sh_vect
+                str_to_write = "Shared Variable ($(sh_var)) Index: $(sh_var.index)\n"
+                # Write the string to the file
+                write(file, str_to_write)
+            end
+        end
+        # Close the file
+        close(file)
+    end
 
     return boundary_linking_vars
 
