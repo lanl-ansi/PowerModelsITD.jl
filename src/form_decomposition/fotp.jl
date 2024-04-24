b2d1566e3603872b069c0c29cb3b8166aa706202
Juan Ospina <jjospina@lanl.gov>
2024-04-24 14:55:09 -0600

    ADD: fotr, fotp, fbs, nfa, and lindist3flow voltage angle constraints. Most of them are improved by using this constraint.

--- src/form_decomposition/fotp.jl
+++ src/form_decomposition/fotp.jl
@@ -22,6 +22,40 @@ function constraint_boundary_voltage_magnitude(pmd::_PMD.FOTPUPowerModel, ::Int,
 
 end
 
+
+"""
+    function constraint_boundary_voltage_angle(
+        pmd::_PMD.FOTPUPowerModel,
+        i::Int,
+        f_idx::Tuple{Int,Int,Int},
+        f_connections::Vector{Int},
+        t_connections::Vector{Int};
+        nw::Int = nw_id_default
+    )
+
+FOTPU boundary bus voltage angle constraints.
+"""
+function constraint_boundary_voltage_angle(pmd::_PMD.FOTPUPowerModel, ::Int, t_bus::Int, ::Vector{Int}, ::Vector{Int}; nw::Int=nw_id_default)
+
+    ## --- NOTE: These constraints seem to make ACP-FOTPU decomposition formulation harder to solve
+    ## if the  _PMD.constraint_mc_theta_ref(pmd_model, i) is kept ---
+
+    va_source = _PMD.var(pmd, nw, :va, t_bus)
+
+    # Add constraint(s): angles
+    JuMP.@constraint(pmd.model, va_source[1] == 0.0)
+
+    # Add constraints related to 120 degrees offset for the distribution b and c phases
+    shift_120degs_rad = deg2rad(120)
+
+    # Offset constraints for other phases (-+120 degrees)
+    JuMP.@constraint(pmd.model, va_source[2] == (va_source[1] - shift_120degs_rad))
+    JuMP.@constraint(pmd.model, va_source[3] == (va_source[1] + shift_120degs_rad))
+
+end
+
+
+
 # TODO: multinetwork compatibility by using nw info.
 """
     function generate_boundary_linking_vars(
@@ -61,7 +95,15 @@ function generate_boundary_linking_vars(pm::_PM.ACPPowerModel, pmd::_PMD.FOTPUPo
     P_load = _PM.var(pm, nw, :pbound_load_scaled, f_idx)
     Q_load = _PM.var(pm, nw, :qbound_load_scaled, f_idx)
 
-    boundary_linking_vars = [[P_load[1], Q_load[1], Vm], [p_aux[1], q_aux[1], vm[1]]]
+    # boundary_linking_vars = [[P_load[1], Q_load[1], Vm], [p_aux[1], q_aux[1], vm[1]]]
+
+    # Distribution: va (subproblem)
+    va = _PMD.var(pmd, nw, :va, t_bus)
+
+    # Transmission: Va (master)
+    Va = _PM.var(pm, nw, :va, f_bus)
+
+    boundary_linking_vars = [[P_load[1], Q_load[1], Vm, Va], [p_aux[1], q_aux[1], vm[1], va[1]]]
 
     if (export_models == true)
         # Open file where shared vars indices are going to be written
